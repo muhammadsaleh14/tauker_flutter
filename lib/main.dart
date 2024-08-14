5c57cbd101c081f39966a4a62bd042c28b54c9b3
muhammadsaleh14 <m.salehjz@gmail.com>
2024-08-14 14:17:43 +0500

    magic email link sign in implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:tauker_mobile/screens/HomeScreen.dart';
-import 'package:tauker_mobile/screens/auth_ui.dart';
-import 'package:tauker_mobile/screens/user_signin.dart';
-import 'package:tauker_mobile/screens/user_signup.dart';
+import 'package:tauker_mobile/pages/account_page.dart';
+import 'package:tauker_mobile/pages/login_page.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   await Supabase.initialize(
-    );
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(MyApp());
 }
+
 final supabase = Supabase.instance.client;
+
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Supabase Auth',
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => UserSignup(),
-        '/homeScreen': (context) => const HomeScreen(),
-      },
-      // home: AuthStateHandler(),
+      title: 'Supabase Flutter',
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.green,
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.green,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.green,
+          ),
+        ),
+      ),
+      home: supabase.auth.currentSession == null
+          ? const LoginPage()
+          : const AccountPage(),
     );
   }
 }
 
-class AuthStateHandler extends StatelessWidget {
-  const AuthStateHandler({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<AuthState>(
-      stream: Supabase.instance.client.auth.onAuthStateChange,
-      builder: (context, snapshot) {
-        if (snapshot.hasData && snapshot.data!.session != null) {
-          return const HomeScreen(); // The screen after successful sign-in
-        } else {
-          return UserSignup(); // The unified authentication screen
-        }
-      },
+extension ContextExtension on BuildContext {
+  void showSnackBar(String message, {bool isError = false}) {
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: isError
+            ? Theme.of(this).colorScheme.error
+            : Theme.of(this).snackBarTheme.backgroundColor,
+      ),
     );
   }
 }
